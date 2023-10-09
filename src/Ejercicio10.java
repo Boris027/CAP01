4238728875fd541ebb8f12e99b5d1248f6b82920
Boris027 <titankiller027@gmail.com>
2023-10-09 09:47:25 +0200

    Ejercicios tema1

---
+++ src/Ejercicio10.java
@@ -0,0 +1,29 @@
+public class Ejercicio10 {
+    public static void main(String[] args) throws Exception {
+
+            
+            System.out.println("\033[32m***************");
+            System.out.println("*             *");
+            System.out.println("*   \033[31m0    0 \033[32m   *                                               ");
+            System.out.println("*      |      *                                          ");
+            System.out.println("*             *                                               ");
+            System.out.println("*  \033[31m(-----) \033[32m   *                                                    ");
+            System.out.println("***************                                            ");
+            System.out.println("                                                                           ");
+            System.out.println("\033[35m ¬                    \033[31m_____    \033[35m0                                                                                                                     ");
+            System.out.println("\033[35m|   |   \033[36m  ______     \033[31m|     )   \033[35m_     \033[36m──────                                                                                                                    ");
+            System.out.println("\033[35m| /      \033[36m|  __  |    \033[31m|____/   \033[35m| |   \033[36m│                                                                                                                    ");
+            System.out.println("\033[35m| ¬      \033[36m| |  | |    \033[31m| \\\\     \033[35m| |   \033[36m ────                                                                                                                    ");
+            System.out.println("\033[35m|   |    \033[36m| |__| |    \033[31m|  \\\\    \033[35m| |   \033[36m     │                                                                                                                    ");
+            System.out.println("\033[35m| /      \033[36m|______|    \033[31m|   \\\\   \033[35m|_|   \033[36m─────                                                                                                                    ");
+
+
+            
+
+
+
+
+
+    }
+   }
+        
---
+++ src/Ejercicio9.java
@@ -0,0 +1,30 @@
+public class Ejercicio9 {
+    public static void main(String[] args) throws Exception {
+
+            
+            System.out.println("***************");
+            System.out.println("*             *");
+            System.out.println("*   0    0    *                                               ");
+            System.out.println("*      |      *                                          ");
+            System.out.println("*             *                                               ");
+            System.out.println("*  (-----)    *                                                    ");
+            System.out.println("***************                                            ");
+            System.out.println("                                                                           ");
+            System.out.println("r ¬                   _____    0                                                                                                                     ");
+            System.out.println("|   |     ______     |     )   _     ──────                                                                                                                    ");
+            System.out.println("| /      |  __  |    |____/   | |   │                                                                                                                    ");
+            System.out.println("| ¬      | |  | |    | \\\\     | |    ────                                                                                                                    ");
+            System.out.println("|   |    | |__| |    |  \\\\    | |        │                                                                                                                    ");
+            System.out.println("| /      |______|    |   \\\\   |_|   ─────                                                                                                                    ");
+
+
+
+            
+
+
+
+
+
+    }
+   }
+        
